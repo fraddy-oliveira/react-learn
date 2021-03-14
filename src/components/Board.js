b49d87b47447c37d29871d65843e4a9a574820e8
Fraddy Oliveira <fraddy071@gmail.com>
2021-03-14 14:52:39 +0530

    Refactor Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,36 +2,39 @@ import React from 'react';
 
 import Square from './Square.js';
 
+const range = (start, stop, step) =>
+  Array.from({length: (stop - start) / step + 1}, (_, i) => start + i * step);
+
 class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         box_value={this.props.squares[i]}
         squareClick={() => this.props.onSquareClick(i)}
       />
     );
   }
 
-  render() {
+  renderBoard(rows = 3, cols = 3) {
+    let rowsArray = range(1, rows, 1);
+    let colsArray = range(1, cols, 1);
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {colsArray.map((col) => {
+          return (
+            <div className="board-row" key={col}>
+              {rowsArray.map((row) => this.renderSquare(row - 1))}
+            </div>
+          );
+        })}
       </div>
     );
+  }
+
+  render() {
+    return this.renderBoard(3, 3);
   }
 }
 
